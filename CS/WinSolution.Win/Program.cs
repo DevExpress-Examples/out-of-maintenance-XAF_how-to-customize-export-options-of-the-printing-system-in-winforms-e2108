6a2076370e00551f1a9ef888f3e84be057e2873b
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WinSolution.Win/Program.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Configuration;
+using System.Windows.Forms;
+using DevExpress.ExpressApp.Security;
+using WinSolution.Module;
+
+namespace WinSolution.Win {
+    static class Program {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main() {
+#if EASYTEST
+			DevExpress.ExpressApp.Win.EasyTest.EasyTestRemotingRegistration.Register();
+#endif
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            EditModelPermission.AlwaysGranted = System.Diagnostics.Debugger.IsAttached;
+            MyDetailsController.CanGenerateMyDetailsNavigationItem = true;
+            ObjectAccessComparer.AllowModifyCurrentUserObjectDefault = true;
+            ObjectAccessComparer.AllowNavigateToCurrentUserObjectDefault = true;
+            WinSolutionWindowsFormsApplication winApplication = new WinSolutionWindowsFormsApplication();
+#if EASYTEST
+			if(ConfigurationManager.ConnectionStrings["EasyTestConnectionString"] != null) {
+				winApplication.ConnectionString = ConfigurationManager.ConnectionStrings["EasyTestConnectionString"].ConnectionString;
+			}
+#endif
+            winApplication.ConnectionString = CodeCentralExampleInMemoryDataStoreProvider.ConnectionString;
+            if (ConfigurationManager.ConnectionStrings["ConnectionString"] != null) {
+                winApplication.ConnectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+            }
+            try {
+                DevExpress.ExpressApp.Xpo.InMemoryDataStoreProvider.Register();
+                                winApplication.ConnectionString = DevExpress.ExpressApp.Xpo.InMemoryDataStoreProvider.ConnectionString;
+                winApplication.Setup();
+                winApplication.Start();
+            } catch (Exception e) {
+                winApplication.HandleException(e);
+            }
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,10 @@
+# How to Customize Export Options of the Printing System in WinForms
+
+
+<p>This example demonstrates how to access and customize the <a href="http://documentation.devexpress.com/#WindowsForms/DevExpressXtraPrintingPrintingSystemMembersTopicAll">PrintingSystem</a> object, which is responsible for the print preview and export settings. In order to do this, the PrintingSettingsLoaded and CustomShowPreview events of the PrintingController and WinReportServiceController are handled respectively. Refer to the <a href="http://documentation.devexpress.com/#Xaf/CustomDocument3283">How to: Customize Export Options of the Printing System</a> topic for details.</p>
+<p><strong>IMPORTANT NOTE</strong><br /> You can also customize some print preview settings via the corresponding properties of the report object in the report end-user designer. Refer to the <a href="http://documentation.devexpress.com/#XtraReports/DevExpressXtraReportsUIXtraReportMembersTopicAll">XtraReport Members</a> help topic for more details.</p>
+<p><strong>See Also:</strong><br /><a href="https://www.devexpress.com/Support/Center/p/E1855">How to: Implement a Custom Report Class</a></p>
+
+<br/>
+
+
